289a9ffcfc36db5715c3d4a1b96ce83a3ce371c8
JohnAsare <johnasare7@gmail.com>
2018-04-27 19:23:58 -0400

    Implemented queue class

--- Queue.py
+++ Queue.py
@@ -5,3 +5,23 @@
 
 """ Queue implemntation """
 
+
+class Queue:
+
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, item):
+        self.queue.insert(0, item)
+
+    def dequeue(self):
+        return self.queue.pop()
+
+    def peek(self):
+        return self.queue[-1]
+
+    def isEmpty(self):
+        return self.queue == []
+
+    def size(self):
+        return len(self.queue)
