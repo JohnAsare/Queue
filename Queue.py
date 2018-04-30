287981807712cb700951c06bb915928a2eb645cd
JohnAsare <johnasare7@gmail.com>
2018-04-30 00:54:20 -0400

    I finished queue implementation. I am a pro now

--- Queue.py
+++ Queue.py
@@ -21,7 +21,24 @@ class Queue:
         return self.queue[-1]
 
     def isEmpty(self):
-        return self.queue == []
+        return self.queue == 0
 
     def size(self):
         return len(self.queue)
+
+
+if __name__ == "__main__":
+    line = Queue()
+    line.enqueue("John")
+    line.enqueue("Tom")
+    line.enqueue("Sam")
+    print('\n', "People standing on the line are: ", line.queue)
+
+    line.enqueue("Mariam")
+    line.enqueue("Liam")
+    print('\n', "the line has expanded, these are the people now: ", line.queue)
+    line.dequeue()
+    line.dequeue()
+    print('\n', "now there are these people on line? ", line.queue ,'\n')
+    print("Is anyone on the line",line.isEmpty(), '\n',"How many people are on the line",
+          line.size(), '\n',"Who is the last person on the line? ", line.peek())
